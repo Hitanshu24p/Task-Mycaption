3815094e0f52c418b603477ca3b8e3178e37a474
Hitanshu24p <83834384+Hitanshu24p@users.noreply.github.com>
2021-05-11 23:20:54 +0530

    Done by Hitanshu
    Submitting the Task 1 question number 2.
    product of two numbers.

--- Task 1(2).cpp
+++ Task 1(2).cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
 	double num1, num2, product;
-	cout << "Enter any two number"; //plz enter 3.4 and 5.5 as asked in the question
+	cout << "Enter any two number"; 
 	cin>> num1 >> num2;
 	product= num1*num2;
 	cout << " product = "<< product; 
