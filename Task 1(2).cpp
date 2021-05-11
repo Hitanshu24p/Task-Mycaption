9685ffae418a5009f8eab9b4618dab5217fc45aa
Hitanshu24p <83834384+Hitanshu24p@users.noreply.github.com>
2021-05-11 23:16:57 +0530

    Done by Hitanshu
    Submitting of Task 1 question number 2.

---
+++ Task 1(2).cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double num1, num2, product;
+	cout << "Enter any two number"; //plz enter 3.4 and 5.5 as asked in the question
+	cin>> num1 >> num2;
+	product= num1*num2;
+	cout << " product = "<< product; 
+	
+	return 0;
+}
