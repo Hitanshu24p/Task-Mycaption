a0467d452e0ce6d5237040a912ec1ddd2928d2e9
Hitanshu24p <83834384+Hitanshu24p@users.noreply.github.com>
2021-05-20 18:45:49 +0530

    task 2 voting code.
    task 2 voting code.

---
+++ task2 voting.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"enter age of voter : ";
+	cin>>age;
+	
+	if(age>=18)
+	{
+		cout<<"\n you are eligible for voting ";
+		if(age>=55,age<80)
+		{
+			cout<<"\n you are eligible for senior citizen line ";
+		}
+		if(age>=80)
+		{
+			cout<<"\n you dontneed to wait in line direct entery for voting ";
+		}
+	}
+	else
+		cout<<"\n you are under age for voting ";
+	return 0;
+}
